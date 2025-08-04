9c9859d25a5a9831d551b941230a0da98d12521c
Pedro Chambino <pchambino@gmail.com>
2025-08-04 17:32:18 +0100

    v0.0.2 (#3)
    The goal was to add a "collate" functionality so that we can aggregate
    results from our parallel test runs.
    
    But there were a few rubocop and unused code, so ended up trimming down
    the gem to the absolute minium, most things fit into a single file.
    
    This is not a gem that needs much boilerplace, so keep it simple.

--- example/spec/example_spec.rb
+++ example/spec/example_spec.rb
@@ -1,25 +1,25 @@
-require 'spec_helper'
+require "spec_helper"
 
-describe 'ExampleSpecs' do
+describe "ExampleSpecs" do
   let(:test) { 1 }
 
-  it 'succeeds' do
+  it "succeeds" do
     expect(test).to be_an(Integer)
   end
 
-  it 'fails' do
+  it "fails" do
     expect(test).to be_a(String)
   end
 
-  it 'raises' do
+  it "raises" do # rubocop:disable RSpec/NoExpectationExample
     raise ArgumentError
   end
 
-  xit 'should be pending' do
-    pending
+  xit "pending" do # rubocop:disable RSpec/PendingWithoutReason
+    pending "pending"
   end
 
-  it 'retries', :retry_spec do
+  it "retries", :retry_spec do
     expect(test).to be_a(String)
   end
 end
--- lib/rspec_logfmt_formatter.rb
+++ lib/rspec_logfmt_formatter.rb
@@ -1,2 +1,42 @@
-require 'rspec_logfmt_formatter/formatter'
-require 'rspec_logfmt_formatter/version'
+require "rspec/core"
+require "rspec/core/formatters/base_formatter"
+
+class RspecLogfmtFormatter < RSpec::Core::Formatters::BaseFormatter
+  RSpec::Core::Formatters.register self, :dump_summary
+
+  def self.collate(input)
+    collated = {}
+
+    input.scan(/(tests\.\w+)="(\d+)"/) do |key, value|
+      collated[key] = collated[key].to_i + value.to_i
+    end
+
+    collated.map { |key, value| "#{key}=\"#{value}\"" }.join("\n")
+  end
+
+  def initialize(...)
+    super
+    @retries = []
+  end
+
+  def dump_summary(notification)
+    @summary = notification
+    logfmt_dump
+  end
+
+  def retry(example)
+    @retries << example
+  end
+
+  private
+
+  def logfmt_dump
+    output.puts [
+      %(tests.count="#{@summary.example_count}"),
+      %(tests.skipped="#{@summary.pending_count}"),
+      %(tests.failures="#{@summary.failure_count}"),
+      %(tests.errors="#{@summary.errors_outside_of_examples_count}"),
+      %(tests.retries="#{@retries.count}")
+    ].join("\n")
+  end
+end
